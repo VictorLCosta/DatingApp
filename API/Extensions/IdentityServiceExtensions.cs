9060c68e68cb6744676bce797c155dc106a80eef
VictorLCosta <victorlc2019@outlook.com>
2021-07-23 10:46:41 -0300

    Fixing CORS issue

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -17,7 +17,7 @@ namespace API.Extensions
                     opt.TokenValidationParameters = new TokenValidationParameters
                     {
                         ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"])),
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config.GetValue<string>("TokenKey"))),
                         ValidateAudience = false,
                         ValidateIssuer = false
                     };
