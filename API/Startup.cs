60068c0c9de491c7790de88809e52cea632ebd1f
VictorLCosta <victorlc2019@outlook.com>
2021-07-22 22:01:54 -0300

    Adding extension methods

--- API/Startup.cs
+++ API/Startup.cs
@@ -18,64 +18,48 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using API.Extensions;
 
 namespace API
 {
-public class Startup
-{
-
-private readonly IConfiguration _config;
-
-public Startup(IConfiguration config)
-{
-    _config = config;
-}
-
-// This method gets called by the runtime. Use this method to add services to the container.
-public void ConfigureServices(IServiceCollection services)
-{
-    services.AddScoped<ITokenService, TokenService>();
-            
-    services.AddDbContext<DataContext>(opt => 
+    public class Startup
     {
-        opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-    });
 
-    services.AddControllers();
-    services.AddCors();
-    services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(opt => 
-    {
-        opt.TokenValidationParameters = new TokenValidationParameters
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
         {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["TokenKey"])),
-            ValidateAudience = false,
-            ValidateIssuer = false
-        };
-    });
-}
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-        if (env.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
+            _config = config;
         }
 
-        app.UseHttpsRedirection();
-
-        app.UseRouting();
-        app.UseAuthentication();
-        app.UseAuthorization();
-
-        app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
-
-        app.UseEndpoints(endpoints =>
+        public void ConfigureServices(IServiceCollection services)
         {
-            endpoints.MapControllers();
-        });
+            services.AddApplicationServices(_config);
+            services.AddIdentityServices(_config);
+
+            services.AddControllers();
+            services.AddCors();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
     }
-}
 }
