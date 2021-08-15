6bc0159df8d4e31f492d2aecb1e976ba45efab1d
VictorLCosta <victorlc2019@outlook.com>
2021-08-15 17:20:08 -0300

    Routing member details page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
+import { MemberDetailsComponent } from './members/member-details/member-details.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { NotFoundComponent } from './not-found/not-found.component';
@@ -16,7 +17,7 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     children: [
       {path: "members", component: MemberListComponent, canActivate: [AuthGuard]},
-      {path: "members/:id", component: HomeComponent},
+      {path: "members/:username", component: MemberDetailsComponent},
       {path: "lists", component: ListComponent},
       {path: "messages", component: MessagesComponent}
     ]
--- client/src/app/members/member-details/member-details.component.ts
+++ client/src/app/members/member-details/member-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from 'src/app/_models/member';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-details',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-details.component.css']
 })
 export class MemberDetailsComponent implements OnInit {
+  member: Member
 
-  constructor() { }
+  constructor(private memberService: MembersService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.loadMember();
   }
 
+  loadMember() {
+    this.memberService.getMember(this.route.snapshot.paramMap.get('username'))
+      .subscribe(membro => {
+        this.member = membro
+      })
+  }
 }
