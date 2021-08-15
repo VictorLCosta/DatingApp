5c39cf9de7e137f6b7b5facbb8c80214691f7a35
VictorLCosta <victorlc2019@outlook.com>
2021-08-15 17:00:55 -0300

    Sending the token with interceptor

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -3,12 +3,6 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 
-const httpOptions = {
-  headers: new Headers({
-    Authorization: "Bearer " + JSON.stringify(localStorage.getItem("user"))
-  })
-}
-
 @Injectable({
   providedIn: 'root'
 })
